19829747f29e7d2018c73d0f1250e56810e86a03
Daniel Santos <dfsantosbu@unal.edu.co>
2019-04-14 00:46:24 -0500

    Create readme & basic template. :bird:

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Competitive Programming
+
+## Accounts
+
+### Codeforces xdanielsb https://codeforces.com/submissions/xdanielsb
+### kattis xdanielsb https://open.kattis.com/users/xdanielsb
---
+++ t.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define debug1( x ) cout << #x << " = " <<  x << endl;
+#define debug2( x, y) cout <<#x << " = " << x << " , " <<#y << " = " << y <<endl;
+#define F first
+#define S second
+#define PB push_back
+#define size( x )   int( ( x ).size( ) )
+#define endl '\n'
+#define rep(i, a, b) for( __typeof(a) i =(a); i<(b);i++)
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main(){
+  ios::sync_with_stdio( false );
+  cin.tie( nullptr );
+#ifdef LOCAL
+  freopen("in", "r" , stdin);
+#endif
+ 
+  
+  
+  
+  
+  return 0;
+}
