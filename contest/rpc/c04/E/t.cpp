9e4e3d73946e4ded0a16c7d5759a97c17588fdd1
Daniel <dfsantosbu@unal.edu.co>
2020-06-14 19:17:00 +0200

    add extension solution (counting strings).

--- contest/rpc/c04/E/t.cpp
+++ contest/rpc/c04/E/t.cpp
@@ -18,6 +18,26 @@ typedef vector<int> vi;
 const int mod = 11092019;
 string s;
 
+
+// compute the number of diferent subsequences
+// different in this case means the resulting strings
+// are different
+ll sol( ){
+  int n = size(s);
+  vector< ll > dp(n+1);
+  vector< int > lst(30,-1);
+  dp[0] = 1ll;
+  for( int i = 1; i<=n; i++){
+    dp[i] = dp[i-1] * 2ll;
+    int l = lst[ s[i-1] -'a'] ;
+    if( l != -1){
+      dp[i] = dp[i]  - dp[ l];
+    }
+    lst[s[i-1]-'a'] = i-1;
+  }
+  return dp[n];
+}
+
 int main(){
   ios::sync_with_stdio( false );
   cin.tie( nullptr );
@@ -28,9 +48,15 @@ int main(){
   while( cin >> s){
     ll ans =  1ll;
     map< char, int > cnt;
+    // compute the number of diferent subsequences that are
+    // different, here if there are repited letters, those 
+    // are different, so what makes them different is the index
+    // so two subsequences are different if they have at least one
+    // index of difference
     for( int i= 0; i< size(s); i++)cnt[s[i]]++;
     for( auto x: cnt) ans = (ans * (x.S+1))%mod;
     cout << ans <<endl;
+    /* debug1(sol()); */
   }
 
   return 0;
