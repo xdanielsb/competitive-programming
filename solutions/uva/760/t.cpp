94e8a555db1cd7f45bb57adebebd7898906a228b
Daniel <dfsantosbu@unal.edu.co>
2020-06-15 23:42:15 +0200

    Add solution uva problem (suffix array.)

--- solutions/uva/760/t.cpp
+++ solutions/uva/760/t.cpp
@@ -17,10 +17,18 @@ typedef pair<int, int> ii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 
+/*
+ * Learnings
+ * suffix array can produce repeated lcp, then it is necessary to check thate
+ */
+
 struct entry { ii nr; int p; };
 bool operator <(const entry &a, const entry &b) {
   return a.nr < b.nr;
 }
+
+// code from suffix taken from
+// Bjarki Ágúst Guðmundsson SuprDewd
 struct suffix_array {
   string s;
   int n;
@@ -33,7 +41,8 @@ struct suffix_array {
     for (int stp = 1, cnt = 1; cnt >> 1 < n; stp++, cnt <<= 1){
       P.push_back(vi(n));
       rep(i,0,n)
-        L[L[i].p = i].nr = ii(P[stp - 1][i], i + cnt < n ? P[stp - 1][i + cnt] : -1);
+        L[L[i].p = i].nr = ii(P[stp - 1][i],
+            i + cnt < n ? P[stp - 1][i + cnt] : -1);
       sort(L.begin(), L.end());
       rep(i,0,n)
         P[stp][L[i].p] = i > 0 &&
@@ -50,34 +59,55 @@ struct suffix_array {
     return res;
   }
 };
-// find the number of different substrings of size k
 
 int main(){
   ios::sync_with_stdio( false );
   cin.tie( nullptr );
 #ifdef LOCAL
-  if(!freopen("in", "r" , stdin))
-    cerr << "no file..."<<endl;
+  freopen("in", "r" , stdin);
 #endif
-  int ti;
-  int k, n;
-  string s;
-  cin >> ti;
-  while(ti--){
-    cin >> n >>k;
-    cin >> s;
-    suffix_array sa(s);
-    int ans = n - sa.idx[0] >= k ? 1 : 0;
-    for( int i = 1; i < n; i++){
-      int a = sa.idx[i], b = sa.idx[i-1];
-      int l = sa.lcp(a, b);
-      if( l < k && ( n -sa.idx[i] >= k)){
-        ans++;
+
+  string a, b;
+  int ca = 0;
+  while(cin >> a >> b){
+    if( ca ) cout <<endl;
+    ca++;
+    string t  = a + "#" + b;
+    int n = size(t);
+    int mn = size(a);
+    suffix_array sa(t);
+    int blcp = 0; // greatest lcp
+
+    for( int i = 1; i < n ; i++){
+      int ia = sa.idx[i], ib = sa.idx[i-1];
+      int aux = sa.lcp(ia, ib);
+      if( ia < mn && ib < mn ) continue;
+      if( ia > mn && ib > mn ) continue;
+      if( aux > blcp ){
+        blcp = aux;
       }
     }
-    cout << ans <<endl;
+    if( blcp == 0){
+      cout << "No common sequence.\n";
+      continue;
+    }
+
+    set< string > ans;
+    for( int i= 1; i<n; i++){
+      int ia = sa.idx[i], ib = sa.idx[i-1];
+      if( ia < mn && ib < mn ) continue;
+      if( ia > mn && ib > mn ) continue;
+      string str = "";
+      if( sa.lcp(ia, ib) == blcp ){
+        for( int j = ia; j < ia+blcp; j++)
+          str.push_back(t[j]);
+        ans.insert(str);
+      }
+    }
+    for( string x: ans)
+      cout << x <<endl;
   }
-
-
+  
+  
   return 0;
 }
