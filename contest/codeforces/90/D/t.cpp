dd03df2e56914afd3269b39df759b4f33a76db4b
Daniel <dfsantosbu@unal.edu.co>
2020-07-10 20:10:07 +0200

    Add correction last exercise, < nice greedy.

--- contest/codeforces/90/D/t.cpp
+++ contest/codeforces/90/D/t.cpp
@@ -16,15 +16,6 @@ typedef long long ll;
 typedef pair<int, int> ii;
 typedef vector<int> vi;
 
-ll find( vector< ll > A, int l, int r){
-  ll ans = 0;
-  if( l < 0 || r >= size(A)) return 0;
-  while( l < r ){
-    swap(A[l++], A[r--]);
-  }
-  for( int i= 0; i < size(A); i+=2) ans +=abs(A[i]);
-  return ans;
-}
 
 int dx[8] = {-2, -1, 1, 2, 0, 0, 0, 0};
 int dy[8] = {0, 0,  0, 0, -2, -1, 1, 2};
@@ -42,31 +33,27 @@ int main(){
   while(t--){
     int n;
     cin >> n;
-    vector< ll > A(n);
+    vector< int > A(n);
+    ll ans = 0;
     for( int i = 0; i < n; i++){
-      cin >> A[i];
-      if( i % 2 == 0){
-        A[i] *= -1;
-      }
+      cin >>A[i];
+      if( i % 2 == 0) ans += A[i];
     }
-    // find the limits of the maximun subsequent subarray
-    int l = 0, r = 0;
-    ll maxsum = A[0], cursum = A[0];
-    for( int i = 0; i < n; i++){
-      if(A[i] > A[i] + cursum){
-        l = i;
-      }
-      cursum = max( cursum+A[i], A[i]);
-      if( cursum > maxsum){
-        r = i;
-        maxsum = cursum;
-      }
-    }
-    ll ans = find(A, l, r);
-    for( int i = 0; i < 8; i++)
-      ans = max(ans, find(A, l+dx[i], r+dy[i]));
-    cout << ans <<endl;
 
+    ll inc = 0; // max possible increment that we can generate
+    ll aux = 0;
+    for( int j = 0;  j < n-1; j+=2){
+      aux += A[j+1] - A[j];
+      if( aux  < 0 ) aux = 0;
+      inc = max( aux, inc);
+    }
+    aux = 0;
+    for( int j = 1; j < n-1; j+=2){
+      aux += A[j] - A[j+1];
+      if( aux < 0) aux = 0;
+      inc = max( aux, inc);
+    }
+    cout << ans + inc <<endl;
   }
   
   return 0;
