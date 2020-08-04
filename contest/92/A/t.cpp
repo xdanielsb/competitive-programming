dc0b28af6aa67937ce3ca9587997d38de1a8117b
Daniel <dfsantosbu@unal.edu.co>
2020-08-04 23:23:45 +0200

    add solution easy problem codeforces

---
+++ contest/92/A/t.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define d(x) cout <<#x << " = " << x << ", ";
+#define d1( x ) d(x); cout <<endl;
+#define d2( x, y ) d(x); d(y); cout <<endl;
+#define d3( x, y, z ) d(x); d(y); d(z); cout <<endl;
+#define F first
+#define S second
+#define PB push_back
+#define size( x )   int( ( x ).size( ) )
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main(){
+  ios::sync_with_stdio( false );
+  cin.tie( nullptr );
+#ifdef LOCAL
+  freopen("in", "r" , stdin);
+#endif
+  int t;
+  cin >> t;
+  while(t--){
+    int l, r;
+    cin >> l >> r;
+    if(l*2 > r ){
+      cout << -1 <<" " <<-1 <<endl;
+    }else{
+      cout << l << " " << (l*2)<< endl;
+    }
+
+  }
+ 
+  return 0;
+}
