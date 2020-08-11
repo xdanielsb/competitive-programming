38f0a3f375efcc36e3f9df647d9c47c1250c422d
Daniel <dfsantosbu@unal.edu.co>
2020-08-11 23:35:36 +0200

    Add solution problem.

---
+++ contest/codeforces/662/A/t.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define d(x) cout <<#x << " = " << x << ", ";
+#define d1( x ) d(x); cout <<endl;
+#define d2( x, y ) d(x); d(y); cout <<endl;
+#define d3( x, y, z ) d(x); d(y); d(z); cout <<endl;
+#define F first
+#define S second
+#define PB push_back
+#define size( x )   int( ( x ).size( ) )
+#define endl '\n'
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+
+int main(){
+  ios::sync_with_stdio( false );
+  cin.tie( nullptr );
+#ifdef LOCAL
+  freopen("in", "r" , stdin);
+#endif
+
+
+  int t;
+  cin >> t;
+  while(t--){
+    int n;
+    cin >> n;
+    if( n == 1) cout << 1;
+    else if (n == 2) cout << 2;
+    else cout << (n/2 + 1);
+    cout <<endl;
+  }
+  return 0;
+}
