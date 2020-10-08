218ba1953515b16a3fa34823782fca8f6cc0c3a2
Daniel <dfsantosbu@unal.edu.co>
2020-10-08 21:17:46 +0200

    Rename folder.

---
+++ solutions/kylie/trie_valid_word_dict/t.cpp
@@ -0,0 +1,4 @@
+
+// given a list of words like {"i", "love", "apples","app"}
+// and an input like "iloveapp" you have to check if you can
+// split that sentence into valid word in the dictionary.
